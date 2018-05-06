f96804df43d9d0c61324249b1df4a9d4872a7ed8
Douglas Crockford <douglas@crockford.com>
2018-05-06 04:49:28 -0700

    demo

---
+++ demo.js
@@ -0,0 +1,139 @@
+// demo.js
+
+// This is used by demo.html to demonstrate parseq.js. It include a widget
+// function that represents a service factory, a show callback that displays the
+// final result, and an parseq routine written as an annotated nested array.
+
+// This interacts with the browser with Plain Old DOM.
+
+/*jslint browser */
+
+/*property
+    addEventListener, appendChild, backgroundColor, body, createElement,
+    createTextNode, fallback, getElementById, location, onclick, parallel,
+    race, reload, sequence, stringify, style, type, value
+*/
+
+import parseq from "./parseq.js";
+
+document.getElementById("reset").onclick = function (ignore) {
+    window.location.reload(true);
+};
+
+function widget(name) {
+    return function widget_requestor(callback, value) {
+        let result = (value !== undefined)
+            ? value + ">" + name
+            : name;
+        let demo = document.getElementById("demo");
+        let fieldset = document.createElement("fieldset");
+        let legend = document.createElement("legend");
+        let success = document.createElement("input");
+        let failure = document.createElement("input");
+        fieldset.appendChild(legend);
+        fieldset.appendChild(success);
+        fieldset.appendChild(failure);
+        legend.appendChild(document.createTextNode(name));
+        success.type = "button";
+        success.value = "success";
+        success.addEventListener(
+            "click",
+            function success_handler() {
+                fieldset.style.backgroundColor = "lightgreen";
+                return callback(result);
+            },
+            false
+        );
+        failure.type = "button";
+        failure.value = "failure";
+        failure.addEventListener(
+            "click",
+            function failure_handler() {
+                fieldset.style.backgroundColor = "pink";
+                return callback(undefined, result);
+            },
+            false
+        );
+        demo.appendChild(fieldset);
+        return function widget_cancel() {
+            fieldset.style.backgroundColor = "darkgray";
+        };
+    };
+}
+
+function show(value, reason) {
+    let result;
+    let title;
+    let color;
+    let body;
+    let demo = document.getElementById("demo");
+    let fieldset = document.createElement("fieldset");
+    let legend = document.createElement("legend");
+    if (value !== undefined) {
+        result = JSON.stringify(value);
+        title = "success";
+        color = "lightgreen";
+        body = "mintcream";
+    } else {
+        result = JSON.stringify(reason);
+        title = "failure";
+        color = "pink";
+        body = "mistyrose";
+    }
+    fieldset.appendChild(legend);
+    legend.appendChild(document.createTextNode(title));
+    fieldset.appendChild(document.createTextNode(result));
+    fieldset.style.backgroundColor = color;
+    legend.style.backgroundColor = color;
+    document.body.style.backgroundColor = body;
+    demo.appendChild(fieldset);
+}
+
+parseq.parallel(
+    [
+        widget("A"),
+        parseq.race([
+            widget("Race B1"),
+            widget("Race B2"),
+            widget("Race B3")
+        ]),
+        parseq.sequence([
+            widget("Seq C1"),
+            widget("Seq C2"),
+            widget("Seq C3")
+        ]),
+        parseq.sequence([
+            widget("Seq D1"),
+            widget("Seq D2"),
+            widget("Seq D3")
+        ]),
+        parseq.fallback([
+            widget("Fall F1"),
+            widget("Fall F2"),
+            widget("Fall F3")
+        ])
+    ],
+    [
+        widget("Opt Q"),
+        parseq.race([
+            widget("Opt Race R1"),
+            widget("Opt Race R2"),
+            widget("Opt Race R3")
+        ]),
+        parseq.sequence([
+            widget("Opt Seq S1"),
+            widget("Opt Seq S2"),
+            widget("Opt Seq S3")
+        ]),
+        parseq.sequence([
+            widget("Opt Seq T1"),
+            widget("Opt Seq T2"),
+            widget("Opt Seq T3")
+        ]),
+        parseq.fallback([
+            widget("Opt Fall V1"),
+            widget("Opt Fall V2"),
+            widget("Opt Fall V3")
+        ])
+    ]
+)(show);
