9f34a0d762e427ed9252d473bafc90945f90b1f1
Atef Ben Ali <atefBB@users.noreply.github.com>
2019-06-30 13:29:14 +0200

    Fix typo

--- demo.js
+++ demo.js
@@ -2,7 +2,7 @@
 
 // This is used by demo.html to demonstrate parseq.js. It includes a widget
 // function that represents a service factory, a show callback that displays the
-// final result, and an parseq routine written as an annotated nested array.
+// final result, and a parseq routine written as an annotated nested array.
 
 // This interacts with the browser with Plain Old DOM.
 
